efd34ae2ca348ea95baf96ede8d4066245bfadb4
waikau maina <ericmaina.dev@gmail.com>
2023-05-31 18:13:17 +0300

    added corsheaders to the app

--- notes/serializers.py
+++ notes/serializers.py
@@ -5,6 +5,7 @@ class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = (
+            "id",
             "title",
             "content",
         )
